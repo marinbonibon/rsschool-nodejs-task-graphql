5f1f4be5aa2ba18808f68b3c3140a1b2f68dc781
Maryna Petrenko <marinbonibon@gmail.com>
2023-07-20 00:33:28 +0200

    feat: implemented query for memberTypes

--- src/routes/graphql/schemas.ts
+++ src/routes/graphql/schemas.ts
@@ -1,4 +1,5 @@
 import { Type } from '@fastify/type-provider-typebox';
+import { GraphQLFloat, GraphQLInt, GraphQLList, GraphQLObjectType, GraphQLSchema, GraphQLString } from 'graphql';
 
 export const gqlResponseSchema = Type.Partial(
   Type.Object({
@@ -18,3 +19,30 @@ export const createGqlResponseSchema = {
     },
   ),
 };
+
+const MemberType = new GraphQLObjectType({
+    name: 'Member',
+    fields: () => ({
+        id: {type: GraphQLString},
+        discount: {type: GraphQLFloat},
+        postsLimitPerMonth: {type: GraphQLInt},
+    }),
+})
+
+const RootQueryType = new GraphQLObjectType({
+    name: 'RootQuery',
+    fields: () => ({
+        memberTypes: {
+            type: new GraphQLList(MemberType),
+            args: {},
+            async resolve(obj, args, prisma, info) {
+                return await prisma.memberType.findMany();
+            }
+        },
+    }),
+
+})
+
+export const schema = new GraphQLSchema({
+    query: RootQueryType
+})
